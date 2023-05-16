92994c1986015a8a3fea1aea41c6e495fd65de7f
Abishek Arun <abishek.arun2017@gmail.com>
2023-05-16 20:50:59 +0530

    Changed the layout of the table from grid based to flex based

--- fancy-table/script.js
+++ fancy-table/script.js
@@ -1,22 +1,31 @@
 // const data =
 const mainTable = document.getElementById("main-table");
+const createContainer = (containerClass) => {
+  let container = document.createElement("div");
+  container.className = containerClass;
+  return container;
+};
 const getData = async () => {
   const response = await fetch("https://reqres.in/api/users");
   const responseJSON = await response.json();
   const users = responseJSON.data;
   users.forEach((element) => {
     const { id, email, first_name, last_name, avatar } = element;
-    const textVals = [id, email, first_name, last_name];
-    
-    textVals.forEach((textVal) => {
-      let appendable = document.createElement("p");
-      let textNode = document.createTextNode(textVal);
-      appendable.appendChild(textNode);
-      mainTable.appendChild(appendable);
-    });
-    let image = document.createElement('img')
-    image.src = avatar;
-    mainTable.appendChild(image)
+    let userContainer = createContainer("user");
+    let emailContainer = createContainer("email");
+    let nameContainer = createContainer("name");
+    let pfpContainer = createContainer("pfp");
+    userContainer.appendChild(pfpContainer);
+    userContainer.appendChild(nameContainer);
+    userContainer.appendChild(emailContainer);
+    let pfp = document.createElement("img");
+    pfp.src = avatar;
+    pfpContainer.appendChild(pfp);
+    let name = document.createTextNode(`${first_name} + ${last_name}`);
+    nameContainer.appendChild(name);
+    let userEmail = document.createTextNode(email);
+    emailContainer.appendChild(userEmail);
+    mainTable.appendChild(userContainer)
   });
 };
 
